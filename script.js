577db1b1c122b4bbe69e4cba795a8e7bcb8a9a2b
luis <barrero.luiscarlos@gmail.com>
2021-03-24 10:01:26 -0400

    first commit, and starter files

---
+++ script.js
@@ -0,0 +1,18 @@
+// Pseudo example
+
+// Create an app object (gottaCatchEmAll)
+
+// Initialize preset data in the dedicated properties
+// - apiURL
+// - apiKey
+// - userQuery
+
+// Create a method (getUserQuery) to update the variable (userQuery) based on user input
+
+// Create a method (getPokemon) to make API calls, which takes the user input as a parameter (userQuery)
+// When the API call is successful, display the result by appending the data to the results div
+// If the API call fails, display an error message
+
+// Create an init method to kick off the setup of the application
+// - calls the local method (getPokemon) for a random Pokemon to start
+// - add a 'change' event listener to call the local method (getUserQuery), to track user input
